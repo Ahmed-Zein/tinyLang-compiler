342482d9377743a159da2923d9ec4c6aad253cbb
Ahmed-Zein <ahmedzein3652@gmail.com>
2023-12-27 14:22:00 +0200

    graph

--- ui/graph/GraphDrawer.java
+++ ui/graph/GraphDrawer.java
@@ -26,12 +26,11 @@ public class GraphDrawer {
                 double childY = y;
                 int tmp = 1;
                 for (Node child : ((NonTerminalNode) node).children) {
-                    drawChildLine(pane, x, y, childX, childY);
+                    drawChildLine(pane, x+DEFAULT_NODE_WIDTH/2, y-DEFAULT_NODE_HEIGHT/2 , childX, childY);
                     drawGraph(pane, child, childX, childY, indent + 1);
                     childY += DEFAULT_NODE_HEIGHT * tmp * 2.3  + new Random().nextInt(100);
                     tmp = (child instanceof NonTerminalNode) ? ((NonTerminalNode) (child)).children.size() : 1;
                 }
-
             }
         }
     }
@@ -60,25 +59,6 @@ public class GraphDrawer {
         rectangle.setLayoutX(x);
         rectangle.setLayoutY(y);
     }
-
-//    private void drawChildrenLinesAndNodes(Pane pane, NonTerminalNode node, double x, double y) {
-//    }
-
-//    private void drawChildrenLinesAndNodes(Pane pane, NonTerminalNode nonTerminalNode, double x, double y) {
-//        Random random = new Random();
-//        if (!nonTerminalNode.children.isEmpty()) {
-//            double childX = x - DEFAULT_NODE_WIDTH/ 1.2  + new Random().nextInt(40);
-//            double childY = y + DEFAULT_NODE_HEIGHT + 40 + new Random().nextInt(60 );// Increase vertical spacing between nodes
-//
-//            for (Node child : nonTerminalNode.children) {
-//                drawChildLine(pane, x, y, childX, childY);
-//                drawGraph(pane, child, childX, childY);
-//
-//                // Adjust position for the next child
-//                childX += DEFAULT_NODE_WIDTH + 30 +new Random().nextInt(100); // Adjust horizontal spacing
-//            }
-//        }
-//    }
 
     private void drawChildLine(Pane pane, double parentX, double parentY, double childX, double childY) {
         Line line = new Line(parentX + DEFAULT_NODE_WIDTH / 2, parentY + DEFAULT_NODE_HEIGHT, childX + DEFAULT_NODE_WIDTH / 2, childY);
@@ -133,8 +113,8 @@ class GraphCircularNode extends GraphNode {
 
     private Circle createCircle() {
         Circle circle = new Circle(GraphDrawer.DEFAULT_NODE_WIDTH / 3);
-//        circle.setFill(Color.SNOW);
-        circle.setFill(Color.TRANSPARENT);
+        circle.setFill(Color.SNOW);
+//        circle.setFill(Color.TRANSPARENT);
         circle.setStroke(Color.BLACK);
         return circle;
     }
